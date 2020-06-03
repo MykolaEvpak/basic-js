710f555b3ea29bd7942121cf39f1df71877a33b1
Mykola.Ievpak <Mykola_Ievpak@epam.com>
2020-06-03 16:46:43 +0300

    [Carbon-Dating] Implemented

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+
+/* works but on node.js > 11 
+  return twoDimentionalArray.flat().reduce((total, current) => {
+    return current == "^^" ? ++total : total;
+  }, 0); */
+
+  let result = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    let row = arr[i];
+    
+    for(let j = 0; j < row.length; j++ ){
+      if (arr[i][j] == "^^") {
+        result++;
+      }  
+    }
+  }
+  return result;
+
 };
