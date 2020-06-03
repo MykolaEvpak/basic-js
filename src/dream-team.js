59f16afb27b942da4a8cd4fbe1f94193f39dbabb
Mykola.Ievpak <Mykola_Ievpak@epam.com>
2020-06-03 18:14:26 +0300

    [Dream Team] Implemented

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) { 
+
+  if(Array.isArray(arr)) {
+    let res = [];
+
+    arr.forEach(element => {
+      if (typeof element == 'string' && element.match(/[a-zA-z]+/)) {
+        res.push(element.trim().charAt(0).toUpperCase());
+      }
+    });
+
+    return res.sort().join('');
+  
+  } else return false;
+
 };
