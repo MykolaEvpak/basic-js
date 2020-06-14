c2cb17b1dda8e378b14472c7491175caf8c04ff4
Mykola Ievpak <Mykola_Ievpak@epam.com>
2020-06-14 20:29:16 +0300

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+const DEFAULT_SEPARATOR = "+";
+const DEFAULT_ADDITION_SEPARATOR = "|";
+
+function calculateAddition(options) {
+  let res = "";
+
+  if(options.hasOwnProperty("additionRepeatTimes") && Number.isInteger(options.additionRepeatTimes)) {
+
+    for(let i = 0; i < options.additionRepeatTimes; i++) {
+      res += options.addition;
   
+      if (i !== (options.additionRepeatTimes - 1)) {
+        res += (options.additionSeparator || DEFAULT_ADDITION_SEPARATOR);
+      }
+       
+    }
+  } else return options.addition || "";
+  
+  return res;
+}
+
+module.exports = function repeater(str, options) {
+
+  if(!options) return str;
+
+  let result = "";
+  let addition = calculateAddition(options);
+
+  if(options.hasOwnProperty("repeatTimes") && Number.isInteger(options.repeatTimes)) {
+
+    for(var i = 0; i < options.repeatTimes; i++) {
+      result += str + addition;
+      
+      if (i !== (options.repeatTimes - 1)) {
+        result += (options.separator || DEFAULT_SEPARATOR);
+      }
+  
+    }
+  } else return str + addition;
+
+  return result;
+
+};
+
+// repeater('la', { repeatTimes: 3, separator: 's', addition: '+', additionRepeatTimes: 1 }) //, 'la+sla+sla+');
+// repeater('LALA', { repeatTimes: 3, separator: 's', addition: '++', additionRepeatTimes: 1 }) //, 'LALA++sLALA++sLALA++');
